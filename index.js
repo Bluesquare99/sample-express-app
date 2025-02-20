7ad0e9c3684dbc9400f22ea03e9d1ab2d06bc5a1
Max McCready <75914260+Bluesquare99@users.noreply.github.com>
2025-02-20 10:17:00 -0800

    hello pluto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Venus!</h1>')
+  res.send('<h1>Hello Pluto!</h1>')
 })
 
 const PORT = 8080
